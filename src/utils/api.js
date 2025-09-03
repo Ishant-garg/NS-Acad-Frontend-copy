5cf6cdd218da329cbecd5c517b5ddbbb9971f75e
Ishant Garg <119870492+Ishant-garg@users.noreply.github.com>
2025-09-03 16:54:24 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: 'https://ns-acad-backend.onrender.com/,
   headers: {
     'Content-Type': 'application/json'
   },
